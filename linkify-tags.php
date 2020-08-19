829463835ae1fc890039c701a12292445f1f6dcd
Scott Reilly <scott@coffee2code.com>
2020-08-18 23:55:28 -0700

    Note compatibility through WP 5.5+

--- linkify-tags.php
+++ linkify-tags.php
@@ -10,7 +10,7 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Description: Turn a string, list, or array of tag IDs and/or slugs into a list of links to those tags. Provides a widget and template tag.
  *
- * Compatible with WordPress 3.3 through 5.4+.
+ * Compatible with WordPress 3.3 through 5.5+.
  *
  * =>> Read the accompanying readme.txt file for instructions and documentation.
  * =>> Also, visit the plugin's homepage for additional information and updates.
